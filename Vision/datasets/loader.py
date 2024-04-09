e51c2aad1ac32667c887d6a957ddab4c3683b994
hasanhd555 <hasanhd555@gmail.com>
2024-04-09 15:42:17 +0500

    removed_Clahe_60epoch

--- Vision/datasets/loader.py
+++ Vision/datasets/loader.py
@@ -117,7 +117,7 @@ class PairLoader(Dataset):
 		target_img = read_img(os.path.join(self.root_dir, 'GT', img_name)) * 2 - 1
 
 		# Apply AHE to source and target images
-		source_img = apply_clahe(source_img)
+		source_img = source_img
 		
 		if self.mode == 'train':
 			[source_img, target_img] = augment([source_img, target_img], self.size, self.edge_decay, self.only_h_flip)
@@ -145,6 +145,6 @@ class SingleLoader(Dataset):
 		img = read_img(os.path.join(self.root_dir, img_name)) * 2 - 1
 
 		# Apply AHE to the image
-		img = apply_clahe(img)
+		img = img
 
 		return {'img': hwc_to_chw(img), 'filename': img_name}
